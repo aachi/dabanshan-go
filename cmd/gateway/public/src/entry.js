755914dc9ace30e8dc289b64615fd4ca278b1405
laidingqing <laidingqing@gmail.com>
2017-11-09 10:29:23 +0800

    html styles for login.

--- cmd/gateway/public/src/entry.js
+++ cmd/gateway/public/src/entry.js
@@ -1,11 +1,10 @@
 'use strict';
-
 require(['angular', './controllers', 'angular-route'], 
   function(angular, controllers) {
-    angular.module('app', ['ngRoute'])
+    angular.module('app', ['ngRoute', 'ui.bootstrap'])
       .config(['$routeProvider', '$httpProvider', function ($routeProvider, $httpProvider) {
-        $routeProvider.otherwise({redirectTo: '/home'});
-        $routeProvider.when('/home', {templateUrl: 'views/home.html', controller: controllers.HomeCtrl})
+        $routeProvider.otherwise({redirectTo: '/login'});
+        $routeProvider.when('/login', {templateUrl: 'views/login.html', controller: controllers.HomeCtrl})
     }])
     angular.bootstrap(document, ['app']); 
 });
--- cmd/gateway/public/webpack.dev.config.js
+++ cmd/gateway/public/webpack.dev.config.js
@@ -4,21 +4,52 @@ var path = require('path');
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
-    
+var CopyWebpackPlugin = require('copy-webpack-plugin');
 /**
  * Module dependencies
  */
 module.exports = {
     cache: false,
+
     entry: {
-        'app': __dirname + '/src/entry',
+        angular: [
+            './node_modules/angular/angular',
+            './node_modules/angular-route/angular-route'
+        ],
+        bootstrap: './node_modules/angular-ui-bootstrap/dist/ui-bootstrap-tpls',
+        main: __dirname + '/src/entry',
     },
 
     output: {
         path: 'dist/',
         publicPath: 'dist/',
-        filename: '[name].js',
-        chunkFilename: '[chunkhash].js'
+        filename: '[name].bundle.js'
     },
+
+    // module:{
+    //     rules: [{
+    //         test: /\.css$/,
+    //         use: ExtractTextPlugin.extract({
+    //             fallback: "style-loader",
+    //             use: [{
+    //                 loader: "css-loader"
+    //             },],
+    //         })
+    //     }]
+    // },
+
+    plugins: [
+        // new ExtractTextPlugin('styles/main.css'),
+        // new webpack.optimize.UglifyJsPlugin({
+        //   sourceMap: true
+        // }),
+        new webpack.optimize.CommonsChunkPlugin({
+          names: [ 'bootstrap', 'angular' ]
+        }),
+        new CopyWebpackPlugin([
+          { from: 'node_modules/bootstrap/dist/css/bootstrap.min.css', to: 'bootstrap/css' },
+          { from: 'node_modules/bootstrap/dist/fonts', to: 'bootstrap/fonts' },
+        ])
+      ]
     //watch:true
 };
